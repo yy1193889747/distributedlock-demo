f1e02052de0019edecf66ea6d386487500e3574a
unknown <congyang.guo@aicai.com>
2018-05-30 16:11:58 +0800

    Initial commit

---
+++ src/main/java/com/cy/distributedlockdemo/DistributedlockDemoApplication.java
@@ -0,0 +1,12 @@
+package com.cy.distributedlockdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedlockDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedlockDemoApplication.class, args);
+    }
+}
